f2086c17a82fe8da2f15913dd69c0ce3a1b26082
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-07-23 23:53:44 -0300

    Adiciona reducer de adição ao carrinho

--- src/store/modules/card/reducer.js
+++ src/store/modules/card/reducer.js
@@ -1,8 +1,19 @@
+import produce from 'immer';
+
 export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      console.log('....');
-      return [...state, action.product];
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.product.id);
+        if (productIndex >= 0) {
+          draft[productIndex].amount += 1;
+        } else {
+          draft.push({
+            ...action.product,
+            amount: 1,
+          });
+        }
+      });
     default:
       return state;
   }
